46e0c349ab4e561965fcfb852be14a1ba7d727bb
Nimesh Bashitha <2022csc023@univ.jfn.ac.lk>
2025-02-26 08:01:24 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,12 @@
-<div className="md:text-3xl p-3 text-3xl items-center justify-center font-semibold" 
-     style={{ height: "75px" }}> {/* Adjust height based on the longest text */}
+import React from 'react';
+import { TypeAnimation } from "react-type-animation";
+import test1 from '../assets/test1.jpg';
+export const Home = () => {
+    return (
+        <div className="md:grid md:grid-cols-2 flex flex-col-reverse gap-3">
+          <div className=" md:mt-20 flex-col md:p-10 items-center gap-6">
+          <div className="md:text-3xl p-3 text-4xl items-center justify-center font-semibold" 
+     style={{ height: "85px" }}> {/* Adjust height based on the longest text */}
   <TypeAnimation
     sequence={[
       "I am Nimesh Bashitha",
@@ -18,3 +25,20 @@
     repeat={Infinity}
   />
 </div>
+
+            <div className="flex   mt-5 text-lg  md:text-xl font-mono text-home w-5/6 justify-center p-3">
+              I am a Computer Science undergraduate at the University of Jaffna,
+              specializing in    full-stack development and exploring machine learning.
+              Passionate about building innovative solutions and expanding my
+              technical expertise.
+            </div>
+            <div>
+                <button className='rounded-full border-4 border-fuchsia-950 p-3 mt-6 ' style={{background: "#7A1CAC"}}>Download CV</button>
+            </div>
+          </div>
+          <div className='flex md:flex-col justify-center md:items-center'>
+            <img src={test1} className='w-60 h-60 object-contain md:w-96 md:h-96 '></img>
+          </div>
+        </div>
+      );
+}
